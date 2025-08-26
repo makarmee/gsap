2e27a657fd1ddcec9a0a847582b5221a903e19d5
makarmee <mmahdikarimi2006@gmail.com>
2025-08-26 13:04:55 +0430

    setup files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
 import React from 'react'
+
+
+
+gsap.registerPlugin(ScrollTrigger,SplitText)
+
 
 function App() {
   return (
-    <div>App</div>
+    <div className='flex-center h-[100vh]'>
+      <h1 className='text-3xl text-indigo-300'>Hello GSAP</h1>
+    </div>
   )
 }
 
