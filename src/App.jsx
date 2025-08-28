7fb3ce3e4b5e0a6f0e7d1d9ded69f73c54056dc3
makarmee <mmahdikarimi2006@gmail.com>
2025-08-28 16:25:14 +0430

    first part of hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import React from 'react'
 import { NavBar } from './components/NavBar'
+import { Hero } from './components/Hero'
 
 
 
@@ -12,6 +13,8 @@ function App() {
   return (
     <main>
       <NavBar/>
+      <Hero/>
+      <div className='h-dvh bg-black'></div>
     </main>
   )
 }
