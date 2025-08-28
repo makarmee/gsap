360719b8e38419337476654718a26f58ba287ccb
makarmee <mmahdikarimi2006@gmail.com>
2025-08-29 00:04:14 +0430

    contact section added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Cocktails } from './components/Cocktails'
 import { About } from './components/About'
 import { Art } from './components/Art'
 import { Menu } from './components/Menu'
+import { Contact } from './components/Contact'
 
 
 
@@ -22,6 +23,7 @@ function App() {
       <About/>
       <Art/>
       <Menu/>
+      <Contact/>
     </main>
   )
 }
